09d12da034b00ca169a2064609b3aaee57caaf3d
Bryan Lee <bryanmylee@gmail.com>
2020-10-28 22:25:43 +0800

    Create a redeploy script

--- redeploy.sh
+++ redeploy.sh
@@ -4,5 +4,9 @@ git submodule foreach 'git pull'
 mvn clean package -Dmaven.test.skip=true
 # Compose the docker image.
 docker build -t bryanmylee/ryver-api .
+# Push the image
+docker push bryanmylee/ryver-api
+# Redeploy the image on the server
+ssh -i ryver-api-keypair.pem ec2-user@54.255.146.38 '$HOME/redeploy-container.sh; exit'
 # To run the image, use
 # docker run -p 8761:8761 -p 80:8080 --name ryver-api bryanmylee/ryver-api
